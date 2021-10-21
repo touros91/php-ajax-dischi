3d6c231df54a616f19f086a3e48a64e917b17209
touros91 <andreageraci91@gmail.com>
2021-10-21 15:00:32 +0200

    Aggiunto prima Milestone completa

---
+++ data.php
@@ -0,0 +1,67 @@
+<?php
+$database = [
+	[
+		'title' => 'New Jersey',
+		'author' => 'Bon Jovi',
+		'year' => 1988,
+		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+	],
+	[
+		'title' => 'Live at Wembley 86',
+		'author' => 'Queen',
+		'year' => 1992,
+		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
+	],
+	[
+		'title' => 'Ten\'s Summoner\'s Tales',
+		'author' => 'Sting',
+		'year' => 1993,
+		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
+	],
+	[
+		'title' => 'Steve Gadd band',
+		'author' => 'Steve Gadd Band',
+		'year' => 2018,
+		'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
+	],
+	[
+		'title' => 'Brave new World',
+		'author' => 'Iron Maiden',
+		'year' => 2000,
+		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg'
+	],
+	[
+		'title' => 'One more car, one more rider',
+		'author' => 'Eric Clapton',
+		'year' => 2002,
+		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+	],
+	[
+		'poster' => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+		'title' => "Made in Japan",
+		'author' => "Deep Purple",
+		'genre' => "Rock",
+		'year' => 1972
+	],
+	[
+		'poster' => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+		'title' => "And Justice for All",
+		'author' => "Metallica",
+		'genre' => "Metal",
+		'year' => 1988
+	],
+	[
+		'poster' => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+		'title' => "Hard Wired",
+		'author' => "Dave Weckl",
+		'genre' => "Jazz",
+		'year' => 1994
+	],
+	[
+		'poster' => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+		'title' => "Bad",
+		'author' => "Michael Jacjson",
+		'genre' => "Pop",
+		'year' => 1987
+	]
+];
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+    require __DIR__ . '/data.php';
+    require __DIR__ . '/functions.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Spotify</title>
+</head>
+<body>
+    <header>
+            <div class="logo">
+                <img src="img/spotify-logo.png" alt="logoAlt">
+            </div>
+    </header>
+    <main>
+        <div class="albums-container">
+            <?php foreach ($database as $album) { ?>
+                <div class="album">
+                    <div class="image">
+                        <img src="<?php echo $album["poster"]; ?>" alt="card.title">
+
+                    </div>
+                    <div class="info">
+                        <h4 class="title"><?php echo strtoupper($album["title"]); ?></h4>
+                        <h5 class="artist"><?php echo $album["author"]; ?></h5>
+                        <h5 class="year"><?php echo $album["year"]; ?></h5>      
+                    </div>        
+                </div>  
+            <?php } ?>
+        </div>
+    </main>
+</body>
+</html>
