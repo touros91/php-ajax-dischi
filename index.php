939c2c347bb6e49e23e32d5474e6321598cd7c96
touros91 <andreageraci91@gmail.com>
2021-10-21 15:12:20 +0200

    Aggiunto Spotify favicon

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Spotify favicon -->
+    <link rel="icon" href="https://icons-for-free.com/iconfiles/png/512/music+round+icon+spotify+icon-1320190507294268936.png">
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
