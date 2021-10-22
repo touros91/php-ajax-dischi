a49ecc37227cb7779beda559a10ad0e6668ac8fd
touros91 <andreageraci91@gmail.com>
2021-10-22 17:56:26 +0200

    Aggiunto Bonus

--- api/index.php
+++ api/index.php
@@ -3,5 +3,21 @@ require __DIR__ . "/../database.php";
 
 header("Content-Type: application/json");
 
-echo json_encode($database);
+if (!empty($_GET["genre"])) {
+
+    $genre = $_GET["genre"];
+    $databaseFiltered = [];
+
+    foreach ($database as $album) {
+        if ($genre == $album["genre"]) {
+            $databaseFiltered[] = $album;
+        }
+    }
+    
+    echo json_encode($databaseFiltered);
+
+} else {
+    echo json_encode($database);
+}
+
 ?>
--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
 const app = new Vue({
     el: "#root",
     data: {
-        albums: null
+        albums: null,
+        genreList: [],
+        selectedValue: ""
     },
     created() {
         axios.get("http://localhost:8888/php-ajax-dischi/api/index.php")
         .then((response) =>
             {
                 this.albums = response.data;
-                console.log(this.albums);
+                this.albums.forEach((element) => {
+                    if (this.genreList.includes(element.genre) == false){
+                        this.genreList.push(element.genre);
+                    }
+                });
             }
         )
         .catch(function(error){
             console.log(error);
         });
+    },
+    methods: {
+        selectGenre() {
+            axios.get("http://localhost:8888/php-ajax-dischi/api/index.php", {
+                params: {
+                  genre: this.selectedValue
+                }
+            })
+            .then((response) =>
+                {
+                    this.albums = response.data;
+                }
+            )
+            .catch(function(error){
+                console.log(error);
+            });
+        }
     }
 });
